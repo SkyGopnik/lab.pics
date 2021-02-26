a0b0ee09ddb2a91376666b8e5a33b9ec16c2b3ff
SkyGopnik <artem.petrunin@list.ru>
2021-02-26 10:19:58 +0300

    update form adaptive

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ export default class extends React.Component {
         <PortfolioSection />
         <ServicesSection />
         <FormSection />
-        <AboutSection />
+        {/*<AboutSection />*/}
         <Footer />
       </>
     )
