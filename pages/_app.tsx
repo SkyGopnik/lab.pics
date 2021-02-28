5a0ca5e3555967e81b73669b2a2ae657d44909fa
Artyom <artem.petrunin@list.ru>
2021-03-01 02:44:46 +0300

    meta fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,8 @@ export default function App ({ Component, pageProps }) {
         <title>Лаборатория</title>
         <link rel="shortcut icon" href="/static/favicon.png" type="image/png" />
         <meta name="viewport" content="width=device-width, user-scalable=no" />
-        <meta name="keywords" content="Студия, дизайн, Lab.pics, лаб пикс, логотипы, мобильные приложения, лендинги, сайт повышенной сложности, оформление соц.сетей, разработка на Tilda, печатная продукция" />
+        <meta name="keywords" content="Студия, дизайн, Lab.pics, лаб пикс, логотипы, мобильные приложения, лендинги, оформление соц.сетей, печатная продукция" />
+        <meta name="description" content="Дружная команда из крутых дизайнеров, способных решить любую Вашу задачу. Мы очень любим дизайн и с трепетом относимся к каждому проекту." />
         <meta property="og:locale" content="ru_RU" />
         <meta property="og:type" content="article" />
         <meta property="og:title" content="Лаборатория" />
