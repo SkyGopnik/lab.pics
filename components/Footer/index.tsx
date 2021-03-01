130ef6a91547205068cf66effcd8dc10e75dd3c9
SkyGopnik <artem.petrunin@list.ru>
2021-03-01 11:08:33 +0300

    add footer anchor

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ScrollableAnchor from 'react-scrollable-anchor';
 
 import style from "./index.module.scss";
 
@@ -11,28 +12,26 @@ export class Footer extends React.Component {
 
   render() {
     return (
-      <footer className={style.footer}>
-        <div className={style.data}>
-          <div className={style.menu}>
-            <a href="#portfolio">Портфолио</a>
-            <a href="#services">Услуги</a>
-            <a href="#about">О нас</a>
+      <ScrollableAnchor id="form">
+        <footer className={style.footer}>
+          <div className={style.data}>
+            <div className={style.menu}>
+              <a href="#portfolio">Портфолио</a>
+              <a href="#services">Услуги</a>
+              <a href="#about">О нас</a>
+            </div>
+            <div className={style.contacts}>
+              <div className={style.title}>Контакты</div>
+              <a href="mailto:design@lab.pics">design@lab.pics</a>
+              <a href="https://www.behance.net/labdes">Behance</a>
+              <a href="https://t.me/labdes">Telegram</a>
+              <a href="https://dribbble.com/labdesign">Dribbble</a>
+              <a href="https://vk.com/lab.dsgn">VK</a>
+            </div>
           </div>
-          <div className={style.contacts}>
-            <div className={style.title}>Контакты</div>
-            <a href="mailto:design@lab.pics">design@lab.pics</a>
-            <a href="https://www.behance.net/labdes">Behance</a>
-            <a href="https://t.me/labdes">Telegram</a>
-            <a href="https://dribbble.com/labdesign">Dribbble</a>
-            <a href="https://vk.com/lab.dsgn">VK</a>
-          </div>
-        </div>
-        <img src="/static/FooterHero.svg" alt="Герой" />
-
-        {/*<div className="container">*/}
-        {/*  1*/}
-        {/*</div>*/}
-      </footer>
+          <img src="/static/FooterHero.svg" alt="Герой" />
+        </footer>
+      </ScrollableAnchor>
     );
   }
 }
