d88f6c54061b65aa998339a6e1f23df81853a61e
SkyGopnik <artem.petrunin@list.ru>
2021-03-01 11:40:12 +0300

    fix main seo

--- components/Pages/MainSection/MainSection.tsx
+++ components/Pages/MainSection/MainSection.tsx
@@ -11,21 +11,15 @@ export class MainSection extends React.Component {
           <div className={style.rightImg} />
           <div className="container">
             <div className={style.content}>
-              <h1>
-                <div className={style.noWordWrap}>
-                  <div>Помогаем</div>
-                  <div>бизнесу с</div>
-                </div>
-                <div className={style.wordWrap}>Помогаем бизнесу с</div>
-                <div><span className={style.year}>2018</span> года</div>
-              </h1>
-              <h3>
-                <div className={style.noWordWrap}>
-                  <div>Реализовали более 300 проектов</div>
-                  <div>и имеем 90% положительных отзывов</div>
-                </div>
-                <div className={style.wordWrap}>Реализовали более 300 проектов и имеем 90% положительных отзывов</div>
-              </h3>
+              <h1>Помогаем бизнесу с <span className={style.year}>2018</span> года</h1>
+              {/*  <div className={style.noWordWrap}>*/}
+              {/*    <div>Помогаем</div>*/}
+              {/*    <div>бизнесу с</div>*/}
+              {/*  </div>*/}
+              {/*  <div className={style.wordWrap}>Помогаем бизнесу с</div>*/}
+              {/*  <div><span className={style.year}>2018</span> года</div>*/}
+              {/*</h1>*/}
+              <h3>Реализовали более 300 проектов и имеем 90% положительных отзывов</h3>
               <div className={style.action}>
                 <a href="#form">
                   <button>Связаться с нами</button>
